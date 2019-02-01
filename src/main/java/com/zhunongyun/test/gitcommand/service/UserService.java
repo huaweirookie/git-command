1ccbcd9ec5e56a6e8ea4e6a0d3a233178934a3eb
oscar <youna790049213@gmail.com>
2019-02-01 10:13:02 +0800

    hotfix test

---
+++ src/main/java/com/zhunongyun/test/gitcommand/service/UserService.java
@@ -0,0 +1,11 @@
+package com.zhunongyun.test.gitcommand.service;
+
+/**
+ * @ClassName UserService
+ * @Description TODO
+ * @Author 悠娜的奶爸
+ * @Date 2019-02-01 10:11
+ * @Version 1.0
+ **/
+public interface UserService {
+}
---
+++ src/main/java/com/zhunongyun/test/gitcommand/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.zhunongyun.test.gitcommand.service.impl;
+
+import com.zhunongyun.test.gitcommand.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName UserServiceImpl
+ * @Description TODO
+ * @Author 悠娜的奶爸
+ * @Date 2019-02-01 10:12
+ * @Version 1.0
+ **/
+@Service
+public class UserServiceImpl implements UserService {
+}
